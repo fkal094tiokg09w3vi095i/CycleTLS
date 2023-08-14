9148b3fc378b2a29b7c2f29671b9c15c3bcd7a7f
ChengHoward <18071131140@sina.cn>
2023-08-14 16:01:37 +0800

    G

--- cycletls/main/tls-test.go
+++ cycletls/main/tls-test.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"github.com/ChengHoward/CycleTLS/cycletls"
+	"io"
 	"log"
 )
 
 func main() {
 
 	client := cycletls.Init()
-	resp, err := client.Do("https://ipinfo.io/0", cycletls.Options{
-		Ja3:       "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0",
+	resp, err := client.Do("https://tls.browserleaks.com/json", cycletls.Options{
+		Ja3:       "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,35-17513-16-51-23-43-11-0-5-27-65281-13-10-45-18-21,29-23-24,0",
 		UserAgent: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0",
-		Proxy:     "socks5://abc:321@127.0.0.1:1087",
+		//Proxy:     "socks5://abc:321@127.0.0.1:1087",
 		Headers: map[string]string{
 			"Accept": "Application/json, text/plain, */*",
 		},
@@ -20,7 +21,9 @@ func main() {
 		log.Print("Request Failed: " + err.Error())
 		return
 	}
+	body, err := io.ReadAll(resp.Body)
+
 	log.Print(resp.Status)
-	log.Print(resp.Body)
+	log.Print(string(body))
 
 }
