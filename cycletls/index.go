2d8a99b9884cb1c12e21d442f51ed5f56b762b5f
ChengHoward <18071131140@sina.cn>
2023-09-08 13:46:38 +0800

    G

--- cycletls/index.go
+++ cycletls/index.go
@@ -2,17 +2,11 @@ package cycletls
 
 import (
 	"encoding/json"
-	"flag"
 	http "github.com/ChengHoward/fhttp"
 	"github.com/ChengHoward/fhttp/http2"
-	"github.com/gorilla/websocket"
 	"io"
-	"io/ioutil"
 	"log"
-	nhttp "net/http"
 	"net/url"
-	"os"
-	"runtime"
 	"strings"
 )
 
@@ -28,7 +22,6 @@ type Options struct {
 	PHeaderOrderKeys []string             `json:"-"`
 	HeaderOrderKeys  []string             `json:"-"`
 	UserAgent        string               `json:"userAgent"`
-	Stream           bool                 `json:"stream"`
 	Proxy            string               `json:"proxy"`
 	Cookies          []Cookie             `json:"cookies"`
 	Timeout          int                  `json:"timeout"`
@@ -37,23 +30,20 @@ type Options struct {
 }
 
 type cycleTLSRequest struct {
-	RequestID string  `json:"requestId"`
-	Options   Options `json:"options"`
 }
 
 // rename to request+client+options
 type fullRequest struct {
 	req     *http.Request
 	client  http.Client
-	options cycleTLSRequest
+	options Options
 }
 
 // Response contains Cycletls response data
 type Response struct {
-	RequestID string
-	Status    int
-	Body      io.ReadCloser
-	Headers   map[string]string
+	Status  int
+	Body    io.ReadCloser
+	Headers map[string]string
 }
 
 // JSONBody converts response body to json
@@ -72,36 +62,34 @@ func (re Response) JSONBody() map[string]interface{} {
 
 // CycleTLS creates full request and response
 type CycleTLS struct {
-	ReqChan  chan fullRequest
-	RespChan chan Response
 }
 
 // ready Request
-func processRequest(request *cycleTLSRequest) (result *fullRequest) {
+func processRequest(options *Options) (result *fullRequest) {
 	var browser = browser{
-		JA3:           request.Options.Ja3,
-		UserAgent:     request.Options.UserAgent,
-		Cookies:       request.Options.Cookies,
-		HTTP2Settings: request.Options.HTTP2Settings,
+		JA3:           options.Ja3,
+		UserAgent:     options.UserAgent,
+		Cookies:       options.Cookies,
+		HTTP2Settings: options.HTTP2Settings,
 	}
 
-	if request.Options.Ja3 != "" && !strings.HasPrefix(request.Options.URL, "https") {
+	if options.Ja3 != "" && !strings.HasPrefix(options.URL, "https") {
 		browser.JA3 = ""
 	}
 
 	client, err := newClient(
 		browser,
-		request.Options.Timeout,
-		request.Options.DisableRedirect,
-		request.Options.UserAgent,
-		request.Options.Proxy,
+		options.Timeout,
+		options.DisableRedirect,
+		options.UserAgent,
+		options.Proxy,
 	)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	req, err := http.NewRequest(strings.ToUpper(request.Options.Method), request.Options.URL, strings.NewReader(request.Options.Body))
+	req, err := http.NewRequest(strings.ToUpper(options.Method), options.URL, strings.NewReader(options.Body))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -135,22 +123,22 @@ func processRequest(request *cycleTLSRequest) (result *fullRequest) {
 	*/
 
 	//ordering the pseudo headers and our normal headers
-	if request.Options.PHeaderOrderKeys == nil {
-		request.Options.PHeaderOrderKeys = []string{":method", ":authority", ":scheme", ":path"}
+	if options.PHeaderOrderKeys == nil {
+		options.PHeaderOrderKeys = []string{":method", ":authority", ":scheme", ":path"}
 	}
 
 	req.Header = http.Header{
-		http.HeaderOrderKey:  request.Options.HeaderOrderKeys,
-		http.PHeaderOrderKey: request.Options.PHeaderOrderKeys,
+		http.HeaderOrderKey:  options.HeaderOrderKeys,
+		http.PHeaderOrderKey: options.PHeaderOrderKeys,
 	}
 	//set our Host header
-	u, err := url.Parse(request.Options.URL)
+	u, err := url.Parse(options.URL)
 	if err != nil {
 		panic(err)
 	}
 
 	//append our normal headers
-	for k, v := range request.Options.Headers {
+	for k, v := range options.Headers {
 		if k != "Content-Length" {
 			req.Header.Set(k, v)
 		}
@@ -158,8 +146,8 @@ func processRequest(request *cycleTLSRequest) (result *fullRequest) {
 	if req.Header.Get("Host") == "" {
 		req.Header.Set("Host", u.Host)
 	}
-	req.Header.Set("user-agent", request.Options.UserAgent)
-	return &fullRequest{req: req, client: client, options: *request}
+	req.Header.Set("user-agent", options.UserAgent)
+	return &fullRequest{req: req, client: client, options: *options}
 
 }
 
@@ -171,24 +159,8 @@ func dispatcher(res *fullRequest) (response Response, err error) {
 
 		headers := make(map[string]string)
 		// parsedError.ErrorMsg + "-> \n" + string(err.Error())
-		return Response{res.options.RequestID, parsedError.StatusCode, ioutil.NopCloser(strings.NewReader(parsedError.ErrorMsg)), headers}, err
+		return Response{parsedError.StatusCode, io.NopCloser(strings.NewReader(parsedError.ErrorMsg)), headers}, err
 
-	}
-
-	encoding := strings.Join(resp.Header["Content-Encoding"], ",")
-	//content := resp.Header["Content-Type"]
-
-	/*bodyBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Print("Parse Bytes" + err.Error())
-		return response, err
-	}*/
-	var Body io.ReadCloser
-	if res.options.Options.Stream {
-		Body = resp.Body
-	} else {
-		defer resp.Body.Close()
-		Body = DecompressBody(resp.Body, encoding)
 	}
 
 	headers := make(map[string]string)
@@ -200,18 +172,8 @@ func dispatcher(res *fullRequest) (response Response, err error) {
 			headers[name] = values[0]
 		}
 	}
-	return Response{res.options.RequestID, resp.StatusCode, Body, headers}, nil
+	return Response{resp.StatusCode, resp.Body, headers}, nil
 
-}
-
-// Queue queues request in worker pool
-func (client CycleTLS) Queue(URL string, options Options, Method string) {
-
-	options.URL = URL
-	options.Method = Method
-	//TODO add timestamp to request
-	opt := cycleTLSRequest{"Queued Request", options}
-	client.ReqChan <- *processRequest(&opt)
 }
 
 // Do creates a single request
@@ -219,9 +181,8 @@ func (client CycleTLS) Do(URL string, options *Options, Method string) (response
 
 	options.URL = URL
 	options.Method = Method
-	opt := cycleTLSRequest{"cycleTLSRequest", *options}
 
-	response, err = dispatcher(processRequest(&opt))
+	response, err = dispatcher(processRequest(options))
 	if err != nil {
 		log.Print("Request Failed: " + err.Error())
 		return response, err
@@ -233,147 +194,6 @@ func (client CycleTLS) Do(URL string, options *Options, Method string) (response
 //TODO rename this
 
 // Init starts the worker pool or returns a empty cycletls struct
-func Init(workers ...bool) CycleTLS {
-	if len(workers) > 0 && workers[0] {
-		reqChan := make(chan fullRequest)
-		respChan := make(chan Response)
-		go workerPool(reqChan, respChan)
-		log.Println("Worker Pool Started")
-
-		return CycleTLS{ReqChan: reqChan, RespChan: respChan}
-	}
+func Init() CycleTLS {
 	return CycleTLS{}
-
-}
-
-// Close closes channels
-func (client CycleTLS) Close() {
-	close(client.ReqChan)
-	close(client.RespChan)
-
-}
-
-// Worker Pool
-func workerPool(reqChan chan fullRequest, respChan chan Response) {
-	//MAX
-	for i := 0; i < 100; i++ {
-		go worker(reqChan, respChan)
-	}
-}
-
-// Worker
-func worker(reqChan chan fullRequest, respChan chan Response) {
-	for res := range reqChan {
-		response, err := dispatcher(&res)
-		if err != nil {
-			log.Print("Request Failed: " + err.Error())
-		}
-		respChan <- response
-	}
-}
-
-func readSocket(reqChan chan fullRequest, c *websocket.Conn) {
-	for {
-		_, message, err := c.ReadMessage()
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				return
-			}
-			log.Print("Socket Error", err)
-			return
-		}
-		request := new(cycleTLSRequest)
-
-		err = json.Unmarshal(message, &request)
-		if err != nil {
-			log.Print("Unmarshal Error", err)
-			return
-		}
-
-		reqChan <- *processRequest(request)
-	}
-}
-
-func writeSocket(respChan chan Response, c *websocket.Conn) {
-	for {
-		select {
-		case r := <-respChan:
-			message, err := json.Marshal(r)
-			if err != nil {
-				log.Print("Marshal Json Failed" + err.Error())
-				continue
-			}
-			err = c.WriteMessage(websocket.TextMessage, message)
-			if err != nil {
-				log.Print("Socket WriteMessage Failed" + err.Error())
-				continue
-			}
-
-		}
-
-	}
-}
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
-func WSEndpoint(w nhttp.ResponseWriter, r *nhttp.Request) {
-	upgrader.CheckOrigin = func(r *nhttp.Request) bool { return true }
-
-	// upgrade this connection to a WebSocket
-	// connection
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		//Golang Received a non-standard request to this port, printing request
-		var data map[string]interface{}
-		bodyBytes, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Print("Invalid Request: Body Read Error" + err.Error())
-		}
-		err = json.Unmarshal(bodyBytes, &data)
-		if err != nil {
-			log.Print("Invalid Request: Json Conversion failed ")
-		}
-		body, err := PrettyStruct(data)
-		if err != nil {
-			log.Print("Invalid Request:", err)
-		}
-		headers, err := PrettyStruct(r.Header)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(headers)
-		log.Println(body)
-	} else {
-		reqChan := make(chan fullRequest)
-		respChan := make(chan Response)
-		go workerPool(reqChan, respChan)
-
-		go readSocket(reqChan, ws)
-		//run as main thread
-		writeSocket(respChan, ws)
-
-	}
-
-}
-
-func setupRoutes() {
-	nhttp.HandleFunc("/", WSEndpoint)
-}
-
-func main() {
-	port, exists := os.LookupEnv("WS_PORT")
-	var addr *string
-	if exists {
-		addr = flag.String("addr", ":"+port, "http service address")
-	} else {
-		addr = flag.String("addr", ":9112", "http service address")
-	}
-
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	setupRoutes()
-	log.Fatal(nhttp.ListenAndServe(*addr, nil))
 }
--- cycletls/main/conc.go
+++ cycletls/main/conc.go
@@ -18,11 +18,9 @@ func main() {
 	// 循环一万次
 	for i := 0; i < 1000; i++ {
 		go func(item int) {
-			resp, err := client.Do(URL, cycletls.Options{
-				Body:      nil,
+			resp, err := client.Do(URL, &cycletls.Options{
 				Ja3:       "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-5-10-11-13-16-18-21-23-27-35-43-45-51-17513-65281,29-23-24,0",
 				UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-				Stream:    true,
 				Timeout:   120,
 				Headers: map[string]string{
 					"x-cb-apikey":   "30e9707df33941e3a5f64f2ea4a19186",
